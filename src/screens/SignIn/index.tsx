7886f9e793cfab5b392097ce2038337c9bb00b6f
Douglas Sena <dsena.m@hotmail.com>
2022-04-17 13:53:39 -0300

    + login anonymously

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,31 +1,29 @@
-import React from 'react';
-
-import { Container, Account, Title, Subtitle } from './styles';
-import { ButtonText } from '../../components/ButtonText';
-import { Button } from '../../components/Button';
-import { Input } from '../../components/Input';
+import React from "react";
+import auth from "@react-native-firebase/auth";
+import { Container, Account, Title, Subtitle } from "./styles";
+import { ButtonText } from "../../components/ButtonText";
+import { Button } from "../../components/Button";
+import { Input } from "../../components/Input";
 
 export function SignIn() {
+  async function handleSignInAnonymously() {
+    const { user } = await auth().signInAnonymously();
+  }
+
   return (
     <Container>
       <Title>MyShopping</Title>
       <Subtitle>monte sua lista de compra te ajudar nas compras</Subtitle>
 
-      <Input
-        placeholder="e-mail"
-        keyboardType="email-address"
-      />
+      <Input placeholder="e-mail" keyboardType="email-address" />
 
-      <Input
-        placeholder="senha"
-        secureTextEntry
-      />
+      <Input placeholder="senha" secureTextEntry />
 
-      <Button title="Entrar" onPress={() => { }} />
+      <Button title="Entrar" onPress={() => {}} />
 
       <Account>
-        <ButtonText title="Recuperar senha" onPress={() => { }} />
-        <ButtonText title="Criar minha conta" onPress={() => { }} />
+        <ButtonText title="Recuperar senha" onPress={() => {}} />
+        <ButtonText title="Criar minha conta" onPress={() => {}} />
       </Account>
     </Container>
   );
