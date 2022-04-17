012670e8de5b6ae795d7767ef589a39670a09bca
Douglas Sena <dsena.m@hotmail.com>
2022-04-17 14:04:09 -0300

    + create user account

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import auth from "@react-native-firebase/auth";
 import { Container, Account, Title, Subtitle } from "./styles";
 import { ButtonText } from "../../components/ButtonText";
 import { Button } from "../../components/Button";
 import { Input } from "../../components/Input";
+import { Alert } from "react-native";
 
 export function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   async function handleSignInAnonymously() {
     const { user } = await auth().signInAnonymously();
+  }
+
+  function handleCreateUserAccount() {
+    auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then(() => Alert.alert("Usuário criado com sucesso"));
   }
 
   return (
@@ -15,16 +25,27 @@ export function SignIn() {
       <Title>MyShopping</Title>
       <Subtitle>monte sua lista de compra te ajudar nas compras</Subtitle>
 
-      <Input placeholder="e-mail" keyboardType="email-address" />
+      <Input
+        placeholder="e-mail"
+        keyboardType="email-address"
+        onChangeText={setEmail}
+      />
 
-      <Input placeholder="senha" secureTextEntry />
+      <Input placeholder="senha" secureTextEntry onChangeText={setPassword} />
 
-      <Button style={{marginBottom: 8}} title="Entrar" onPress={() => {}} />
-      <Button style={{backgroundColor: 'orange'}} title="Entrar como anônimo" onPress={handleSignInAnonymously} />
+      <Button style={{ marginBottom: 8 }} title="Entrar" onPress={() => {}} />
+      <Button
+        style={{ backgroundColor: "orange" }}
+        title="Entrar como anônimo"
+        onPress={handleSignInAnonymously}
+      />
 
       <Account>
         <ButtonText title="Recuperar senha" onPress={() => {}} />
-        <ButtonText title="Criar minha conta" onPress={() => {}} />
+        <ButtonText
+          title="Criar minha conta"
+          onPress={handleCreateUserAccount}
+        />
       </Account>
     </Container>
   );
